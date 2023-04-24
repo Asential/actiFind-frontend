7cf2063a1a1d71c3deb53ff6e8e1c97f0696767c
Asential <dhruvsingh2529@gmail.com>
2023-04-24 19:06:01 +0530

    Version Change, Required Packages & New Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid, Icon } from '@material-ui/core'
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const classes = useStyles();
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center"> ActiFind </Typography>
+                <AcUnitIcon className="image" height="60"></AcUnitIcon>
+            </AppBar>
+            <Grow in>
+                <Container >
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={4}>
+                            <Form />                            
+                        </Grid>  
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                                            
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
