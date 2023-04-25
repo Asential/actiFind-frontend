e8a63f169411bf3a18c2639d032d679e97cc6615
Asential <dhruvsingh2529@gmail.com>
2023-04-25 10:30:05 +0530

    Minor Fixes and Constant Addition

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -44,7 +44,7 @@ const Form = ({currentId, setCurrentId}) => {
                 <TextField name="host" variant="outlined" label="Host" fullWidth value={postData.host} onChange={(e) => setPostData({ ...postData, host: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="description" variant="outlined" label="Description" fullWidth value={postData.description} onChange={(e) => setPostData({ ...postData, description: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
                 <div className={classes.fileInput}><FileBase type="file"multiple = {false}onDone={({base64}) => setPostData({...postData, selectedFile:base64})}/></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submiit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,18 @@
+import { UPDATE, DELETE, FETCH_ALL, CREATE, LIKE} from '../constants/actionTypes';
+
+
 // Function that accepts a posts and an action, performs a logic based on the action.
 const reducerFunction = (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
-        case 'UPDATE':
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post) //Map iterates over an array to change something and returns the updated array.
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post.id !== action.payload)    
-        case 'LIKE':
+        case LIKE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post)
         default:
             return posts;
